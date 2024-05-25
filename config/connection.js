1ee86920e97a9a78233e0870ad4fd8cb600cafd5
klep <klep@kleps-MacBook-Pro.local>
2024-05-25 02:18:55 -0500

    adding connection.js for mongo

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// local or remote cred storage. 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+//export connection string 
+module.exports = connectDB;
