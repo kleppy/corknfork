21315e42b105cd3e467567e6596e43fa4db270c2
klep <klep@kleps-MacBook-Pro.local>
2024-05-29 21:08:13 -0500

    adding cellar model

---
+++ server/models/Cellar.js
@@ -0,0 +1,14 @@
+Const cellarSchema = new Schema({
+  Email: {
+    type: String,
+    required: true,
+    trim: true
+  }, 
+  Pair:[{
+      Food: object.id
+      Wine: object.id
+  }],
+  Wine: object.id,
+  Food: object.id,
+
+
