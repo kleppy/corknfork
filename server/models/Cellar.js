6ee58803db15f7fbb14a56d508dc517f48adb88c
klep <klep@kleps-MacBook-Pro.local>
2024-05-29 21:11:56 -0500

    cellar model

--- server/models/Cellar.js
+++ server/models/Cellar.js
@@ -1,14 +1,21 @@
-Const cellarSchema = new Schema({
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const cellarSchema = new Schema({
   Email: {
     type: String,
     required: true,
     trim: true
   }, 
   Pair:[{
-      Food: object.id
+      Food: object.id,
       Wine: object.id
   }],
   Wine: object.id,
   Food: object.id,
+}); 
 
+const Food = mongoose.model('Cellar', cellarSchema);
 
+module.exports = Cellar;
