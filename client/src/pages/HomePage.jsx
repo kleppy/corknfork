7eb9507128895b4a33b64645f7d6e19cb2691051
Jesse <jessecdenier@gmail.com>
2024-05-29 19:29:46 -0500

    Built the most fundamental home page.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 
-//TODO: Create home page with everything we discussed on 5/29/24.
+// Imports the custom files.
+import pear from "../../public/pear.svg";
+
+//TODO: Make it so each pear icon links to another page or a modal with paired opposites of it's associated list item.
+//TODO: Remove inline CSS from pear icons and build out comprehensive linked CSS file.
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Hello World</p>
+      <div>
+        <h2>Foods</h2>
+        <ul>
+          <li>
+            Food A <img src={pear} alt="Pear Icon" style={{ width: "15px" }} />
+          </li>
+          <li>
+            Food B<img src={pear} alt="Pear Icon" style={{ width: "15px" }} />
+          </li>
+          <li>
+            Food C<img src={pear} alt="Pear Icon" style={{ width: "15px" }} />
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Wines</h2>
+        <ul>
+          <li>
+            Wine A<img src={pear} alt="Pear Icon" style={{ width: "15px" }} />
+          </li>
+          <li>
+            Wine B<img src={pear} alt="Pear Icon" style={{ width: "15px" }} />
+          </li>
+          <li>
+            Wine C<img src={pear} alt="Pear Icon" style={{ width: "15px" }} />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
