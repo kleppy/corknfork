568201a09d95cea481fd9d8bedcc52bfb5fe68cd
Jesse <jessecdenier@gmail.com>
2024-06-03 19:04:12 -0500

    Added basic CSS to the header, and made it so it links to respective pages.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,58 @@
 import React from "react";
-
-// Imports the custom files needed below.
 import CorkNForkLogo from "/corknfork-logo.svg";
 
-// Sets up the structure of the header and navigation bar.
-
-//TODO: Make it so Log Out only appears when you are logged in and vice versa for log in and sign up.
-
 export default function Header() {
+  const isLoggedIn = false; // TODO: Replace with actual authentication logic.
+  // TODO: Confirm that "Log In" and "Sign Up" shows up if user is not logged in, and "Log Out" shows up if user is logged in.
+
   return (
-    <header>
-      <div>
-        <img src={CorkNForkLogo} alt="Cork N Fork Logo" className="w-9" />
-        <h1>CORK N FORK</h1>
+    <header className="bg-gray-800 text-white p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img
+            src={CorkNForkLogo}
+            alt="Cork N Fork Logo"
+            className="w-9 mr-2"
+          />
+          <h1 className="text-xl font-bold">CORK N FORK</h1>
+        </div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className="hover:text-gray-400">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/cellar" className="hover:text-gray-400">
+                Wine Cellar
+              </a>
+            </li>
+            {!isLoggedIn && (
+              <li>
+                <a href="/login" className="hover:text-gray-400">
+                  Log In
+                </a>
+              </li>
+            )}
+            {!isLoggedIn && (
+              <li>
+                <a href="/signup" className="hover:text-gray-400">
+                  Sign Up
+                </a>
+              </li>
+            )}
+            {isLoggedIn && (
+              <li>
+                <a href="/" className="hover:text-gray-400">
+                  Log Out
+                </a>
+                //TODO: Insert logic for signing user out.
+              </li>
+            )}
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Wine Cellar</li>
-          <li>Log In</li>
-          <li>Sign Up</li>
-          <li>Log Out</li>
-        </ul>
-      </nav>
     </header>
   );
 }
