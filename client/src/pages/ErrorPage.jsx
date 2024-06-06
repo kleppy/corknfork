e689502408b4ab3f8675f9d5f9472bd244e7c29e
Chelle Wood <chelleyoungw@gmail.com>
2024-06-06 17:40:06 -0500

    styled css/tailwind for error page. could not get the bg image to show. could get to work only for some images

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-//TODO: Add CSS that makes this page look nice.
-export default function ErrorPage() {
+//TODO: CSS done, if someone wants to try to get image to show up for background, i tried...file name is correct. 
+export default function ErrorPage({ data }) {
   return (
-    <div>
-      <h1>Error Page</h1>
-      <p>Oops! Something went wrong.</p>
-      <Link to="/">Go back to Home</Link>
-    </div>
+    <main className="flex items-center justify-center min-h-screen bg-no-repeat bg-cover bg-[url('../public/spilled-wine.png')]">
+      <div className= "w-full max-w-md">
+        <div className= "bg-rose text-yellow shadow-md rounded-lg p-6">
+          <h4 className= "text-2xl font-semibold mb-6 text-center">Uncorking Error</h4>
+          <div className="card-body">
+              <p className="text-center">
+              Something went wrong while fetching your wine. <br></br>Let's toast to second chances!
+              <br></br>
+              <br></br>
+              <button className="w-full bg-burgundy hover:bg-black text-yellow font-bold py-2 px-4 rounded"><Link to="/">Go back to Home</Link></button>
+              </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
