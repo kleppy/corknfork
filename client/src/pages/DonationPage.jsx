bfc3feae0d70b28e1c10a4cface97b9813ce59d2
Erin <50000677+sinclairems@users.noreply.github.com>
2024-06-06 10:47:46 -0500

    moved functionality of stripe.js to DonationPage.jsx -test

--- client/src/pages/DonationPage.jsx
+++ client/src/pages/DonationPage.jsx
@@ -1,2 +1,56 @@
-import { Link } from "react-router-dom";
-import stripePromise from "../utils/stripe";
+// client/src/components/DonationForm.jsx
+import React, { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+const DonationForm = () => {
+  const [amount, setAmount] = useState(0);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="amount">Amount (USD)</label>
+      <input
+        type="number"
+        id="amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+
+      {/* Stripe Elements */}
+      <div id="card-element">
+        <CardElement />
+      </div>
+
+      <button type="submit" disabled={!stripe || !elements}>
+        Donate
+      </button>
+    </form>
+  );
+};
+
+const DonationPage = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <DonationForm />
+    </Elements>
+  );
+};
+
+export default DonationPage;
