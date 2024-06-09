7b1165122c7ea416f8369d36dd9de9b347414684
Jesse <jessecdenier@gmail.com>
2024-06-09 14:09:10 -0500

    Adjusted npm run build and root level vite.config.js to find index.html.

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+// Import the necessary functions from Vite
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Export the configuration.
+export default defineConfig({
+  // Define plugins to be used by Vite.
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  // Frontend server configuration to establish what the client sees.
+  server: {
+    port: 3000,
+    open: true,
+    // Backend server configuration to handle database API requests.
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+  root: "client", // Specify the root directory if needed
+});
